22a5fc1ffd42de245dfaaf79b617d77658e81a40
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-10-29 11:18:53 -0500

    Comillas magicas

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ trainer = False #Tipo bool
 hobbies = ["Programar", 'Cocinar', """Wow Horda"""] #Tipo array
 direction = (dict) ["country":"Colombia", "state":"Santander"] #Tipo object
 
-
+print(f"""The Theacher's Name is "{name}" """) #Com comillas triples o mágicas puedo usar mas comillas y multiples cosas que quisiera que se vean en consola
