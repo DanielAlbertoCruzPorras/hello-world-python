f7ba0b9464ee088c171652027bc0e578e2bbca42
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-10-29 10:52:12 -0500

    Tipos de datos

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-name = input("Enter your name: ")
-message = f"Hello World {name}"
-print(message)
+
+name = "Daniel Alberto Cruz Porras" #Tipo string
+gender = "M" #Tipo char
+age = 24 #Tipo int
+height = 1.70 #Tipo double
+trainer = False #Tipo bool
+
+#Conjuntos
+hobbies = ["Programar", 'Cocinar', """Wow Horda"""] #Tipo array
+direction = (dict) ["country":"Colombia", "state":"Santander"] #Tipo object
+
+
